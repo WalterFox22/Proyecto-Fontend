2c24c0e6aa9f8953765471f5867a3976d5d23f42
WALTER\Soledad Cobacango <john.cobacango@epn.edu.ec>
2025-01-10 02:25:35 -0500

    creacion de carpetas para roles

---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,56 @@
+import axios from 'axios'
+import { createContext, useEffect, useState} from 'react'
+
+// Creacion del grupo de Whatsapp
+const AuthContext = createContext()
+
+// Creacion del mensaje -> Integrantes(children)
+const AuthProvider = ({children}) =>{
+    const [auth, setAuth] = useState({})
+    // La URL del perfil 
+    const perfil = async (token)=>{
+        try {
+            const url = `${import.meta.env.VITE_BACKEND_URL}/perfil`;
+            
+            // pasos para acceder una una ruta privada
+            const options={
+                headers:{
+                    'Content Type':'application/json',
+                    Authorization:`Bearer${token}`
+                }
+            }
+
+            const respuesta = await axios.get(url, options);
+            setAuth(respuesta.data)
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Para que la informacion no se pierda y se cargo lo que ya se guardo 
+    useEffect(() => {
+        const token = localStorage.getItem('token') // Verificar el token cuando el componente se carga
+        if(token){
+            perfil(token)
+        } 
+    }, []); 
+
+    return (
+        <AuthContext.Provider value={
+            {
+                auth,
+                setAuth              
+            }
+        }>
+            {children}
+        </AuthContext.Provider>
+    )  
+}  
+
+// Exportar para ser invocados 
+export {
+    AuthProvider
+}
+
+export default AuthContext
