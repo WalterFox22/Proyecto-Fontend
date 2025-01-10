d5ecc406c68ba1e1f8ba36401b438c57b66068d0
WALTER\Soledad Cobacango <john.cobacango@epn.edu.ec>
2025-01-10 03:38:50 -0500

    login

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,30 @@
+import Logo from '../assets/EMAUS.png';
+
+const Login =() =>{
+
+    return(
+        <>
+            <div class="text-center">
+                <img src={Logo} class="rounded" alt=""/>
+            </div>
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Correo Electronico</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1"/>
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Ingresar</button>
+            </form> 
+        </>
+    )
+}
+
+export default Login;
