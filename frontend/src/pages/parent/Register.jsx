6f8e15738ad4a395fc7a056bb245715c946628b0
WALTER\Soledad Cobacango <john.cobacango@epn.edu.ec>
2025-01-15 17:40:31 -0500

    registro repre 1.3

--- frontend/src/pages/parent/Register.jsx
+++ frontend/src/pages/parent/Register.jsx
@@ -11,79 +11,74 @@ const Register = () =>{
     return(
         <>
             <Container fluid className="vh-100 p-0">
+                
                 <Row className="h-100 m-0"> 
                     <Col xs={12} md={6} className="p-0">
                     <Image src={Imagen1} className="w-100 h-100" style={{ objectFit: 'cover'}}/>
                     </Col>
                     <Col xs={12} md={6} className="p-0">
-                        <Form>
-                            <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
-                                <Form.Label column sm={2}>
-                                Email
+
+                        <h1 className="text-center my-5 display-4">Registro</h1>
+                        <Form className="mx-auto" style={{ maxWidth: '500px' }}>
+                            <Form.Group as={Row} className="mb-4 align-items-center" controlId="formHorizontalNombre">
+                                <Form.Label column sm={3} className="fw-semibold text-end">
+                                    Nombre
                                 </Form.Label>
-                                <Col sm={10}>
-                                <Form.Control type="email" placeholder="Email" />
+                                <Col sm={9}>
+                                    <Form.Control type="text" className="py-2" />
                                 </Col>
                             </Form.Group>
 
-                            <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
-                                <Form.Label column sm={2}>
-                                Password
+                            <Form.Group as={Row} className="mb-4 align-items-center" controlId="formHorizontalApellido">
+                                <Form.Label column sm={3} className="fw-semibold text-end">
+                                    Apellido
                                 </Form.Label>
-                                <Col sm={10}>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Col sm={9}>
+                                    <Form.Control type="text" className="py-2" />
                                 </Col>
                             </Form.Group>
 
-                            <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
-                                <Form.Label column sm={2}>
-                                Password
+                            <Form.Group as={Row} className="mb-4 align-items-center" controlId="formHorizontalTelefono">
+                                <Form.Label column sm={3} className="fw-semibold text-end">
+                                    Teléfono
                                 </Form.Label>
-                                <Col sm={10}>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Col sm={9}>
+                                    <Form.Control type="text" className="py-2" />
                                 </Col>
                             </Form.Group>
 
-                            <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
-                                <Form.Label column sm={2}>
-                                Password
+                            <Form.Group as={Row} className="mb-4 align-items-center" controlId="formHorizontalCedula">
+                                <Form.Label column sm={3} className="fw-semibold text-end">
+                                    Cédula
                                 </Form.Label>
-                                <Col sm={10}>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Col sm={9}>
+                                    <Form.Control type="text" className="py-2" />
                                 </Col>
                             </Form.Group>
 
-                            <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
-                                <Form.Label column sm={2}>
-                                Password
+                            <Form.Group as={Row} className="mb-4 align-items-center" controlId="formHorizontalEmail">
+                                <Form.Label column sm={3} className="fw-semibold text-end">
+                                    Email
                                 </Form.Label>
-                                <Col sm={10}>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Col sm={9}>
+                                    <Form.Control type="email" placeholder="@" className="py-2" />
                                 </Col>
                             </Form.Group>
 
-                            <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
-                                <Form.Label column sm={2}>
-                                Password
+                            <Form.Group as={Row} className="mb-4 align-items-center" controlId="formHorizontalPassword">
+                                <Form.Label column sm={3} className="fw-semibold text-end">
+                                    Password
                                 </Form.Label>
-                                <Col sm={10}>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Col sm={9}>
+                                    <Form.Control type="password" placeholder="*********" className="py-2" />
                                 </Col>
                             </Form.Group>
 
-                            <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
-                                <Form.Label column sm={2}>
-                                Password
-                                </Form.Label>
-                                <Col sm={10}>
-                                <Form.Control type="password" placeholder="Password" />
-                                </Col>
-                            </Form.Group>
-                            
-
-                            <Form.Group as={Row} className="mb-3">
-                                <Col sm={{ span: 20, offset: 2 }}>
-                                <Button type="submit">Sign in</Button>
+                            <Form.Group as={Row} className="mt-3">
+                                <Col sm={{ span: 12, offset: 0 }} className="text-center">
+                                    <Button type="submit" variant="success" className="btn-lg px-5 py-3" style={{ fontWeight: 'bold', borderRadius: '10px', fontSize: '18px', boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', }}>
+                                        Registrar
+                                    </Button>
                                 </Col>
                             </Form.Group>
                         </Form>
