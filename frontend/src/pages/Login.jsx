a687e30b7168f2d5802e21dfd877217969af3311
WALTER\Soledad Cobacango <john.cobacango@epn.edu.ec>
2025-01-14 21:40:06 -0500

    login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,30 +1,35 @@
 import Logo from '../assets/EMAUS.png';
+import '../App.css';
+
 
 const Login =() =>{
 
     return(
         <>
-            <div class="text-center">
-                <h1>Inicio de Sesión</h1>
-                <img src={Logo} class="rounded" alt=""/>
-            </div>
-            <form>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Correo Electronico</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+           <div class="glass-container">
+                <div class="login-box">
+                    <h2>Login</h2>
+                    <form action='#' method='POST'>
+                        <input type='text' id='username' name='username' required placeholder='Username'/> <br></br>
+                        <input type='password' id='password' name='password' required placeholder='Password'/>
+                        
+                        <div class= "options">
+                            <input type="checkbox" id="remember" name="remember" />
+                            <label for="remember">Recordarme</label>
+                            <a href="#">Olvidaste tu contraseña?</a>
+                        </div>
+
+                        <button type='submit'>Ingresar</button>
+
+                        <p>¿No tienes una cuenta? <a href="#" id='registro'>Registrate</a></p>
+
+                    </form>
+
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1"/>
-                </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Ingresar</button>
-            </form> 
+
+           </div>
         </>
+        
     )
 }
 
